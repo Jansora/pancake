d6c51294d6a378985ad07a9cb582aa941c18ea50
ubuntu <ubuntu@localhost.localdomain>
2019-09-02 17:22:12 +0800

    fix start script

--- backend/src/web/start.sh
+++ backend/src/web/start.sh
@@ -1,1 +1,1 @@
-export GIN_MODE=release && go build main.go && cd ~/ && nohup /go/src/web/main 2&>1 >>/var/log/website/access.log &
+export GIN_MODE=release && go build main.go && nohup ./main 2&>1 >>/var/log/website/access.log &
