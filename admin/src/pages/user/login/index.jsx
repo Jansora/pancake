81e08f81f750522b997c7e59edc641f834ac88ec
18044846 <18044846@cnsuning.com>
2019-09-06 11:40:09 +0800

    fix bugs

--- admin/src/pages/user/login/index.jsx
+++ admin/src/pages/user/login/index.jsx
@@ -3,7 +3,7 @@ import { FormattedMessage, formatMessage } from 'umi-plugin-react/locale';
 import React, { Component } from 'react';
 import Link from 'umi/link';
 import { connect } from 'dva';
-import Login from './components/Login';
+import LoginComponents from './components/Login';
 import styles from './style.less';
 
 const { Tab, UserName, Password, Mobile, Captcha, Submit } = LoginComponents;
