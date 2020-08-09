aad2c955ab0911e14f452d204dc6a4fdab2d702a
Jansora <zhangyue1936@gmail.com>
2020-08-09 20:10:44 +0800

    optimize Docker config

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 sudo docker run -d -p 9191:8081 --name pancake \
--e PG_USERNAME=postgres -e PG_PASSWORD= \
+-e PG_USERNAME=postgres -e PG_PASSWORD=postgres \
 -e PG_CONNECT_URL=jdbc:postgresql://127.0.0.1:5432/ \
 -e OSS_AVAILABLE=false \
 -e OSS_ENDPOINT= \
