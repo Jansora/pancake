ae7a1bd79c5b380bf098e784be30c6b22e599834
Jansora <zhangyue1936@gmail.com>
2020-11-06 11:57:53 +0800

    remove garbage

--- backend/target/classes/sql/init.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO COMMENT
-("id", "parent_id", "user_id", "raw", "owner_type", "create_at", "update_at")
-SELECT 0, 0, 0,  '', 'root', NOW(),  NOW() WHERE NOT EXISTS (SELECT * FROM COMMENT WHERE id = 0 );;
