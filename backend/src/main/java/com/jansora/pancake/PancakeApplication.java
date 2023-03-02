a7b9ce92fb27f6a9825c5390b572b7ce77fc15e9
Jansora <zhangyue1936@gmail.com>
2023-03-02 23:55:50 +0800

    init backend project

---
+++ backend/src/main/java/com/jansora/pancake/PancakeApplication.java
@@ -0,0 +1,13 @@
+package com.jansora.pancake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PancakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PancakeApplication.class, args);
+	}
+
+}
